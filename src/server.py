4d49177fab7c2e3969f9e95647a6b653c7d23188
Dragos Strugar <itsdragos@gmail.com>
2019-06-16 15:02:51 +0200

    feat: setup levelDB

--- src/server.py
+++ src/server.py
@@ -1,9 +1,17 @@
 import sys
 import os
-print('----------------->>>', sys.argv, os.environ['TYPE'])
+import time
+
+print('----------------->>>', sys.argv, os.environ['TYPE'], os.getpid())
+
+if os.environ['TYPE'] == 'master':
+    import plyvel 
+    db = plyvel.DB(os.environ['DB'], create_if_missing=True)
 
 def master(env, start_response):
     start_response('200 OK', [('Content-Type', 'text/html')])
+    print(os.getpid())
+    db.put(b'key-%d' % time.time(), b'bob')
     return [b"Hello, World!"]
 
 
