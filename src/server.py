163eade5184846c4e8e951e8b12c8be74058f5b6
Dragos Strugar <itsdragos@gmail.com>
2019-06-16 15:54:06 +0200

    fix: key found and not in leveldb

--- src/server.py
+++ src/server.py
@@ -1,19 +1,36 @@
 import sys
 import os
 import time
+import hashlib
 
-print('----------------->>>', sys.argv, os.environ['TYPE'], os.getpid())
+# --- MASTER SERVER ---
 
 if os.environ['TYPE'] == 'master':
     import plyvel 
     db = plyvel.DB(os.environ['DB'], create_if_missing=True)
 
 def master(env, start_response):
+    key = env['REQUEST_URI'].encode('utf-8')
+    metakey = db.get(key)
+
+    # key not found
+    if metakey is None:
+        start_response('404 Not Found', [('Content-Type', 'text/html')])
+        return [b"Key not found!"]
+    
+    # key is found
+    metakey = json.loads(metakey)
+    
+
     start_response('200 OK', [('Content-Type', 'text/html')])
-    print(os.getpid())
     db.put(b'key-%d' % time.time(), b'bob')
     return [b"Hello, World!"]
 
+# --- VOLUME SERVER ---
+
+if os.environ['TYPE'] == 'volume':
+    import socket
+    host = socket.gethostname()
 
 def volume(env, start_response):
     start_response('200 OK', [('Content-Type', 'text/html')])
