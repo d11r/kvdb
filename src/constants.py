6c19e97abd33356d73339775fda6b1e709c94826
Dragos Strugar <itsdragos@gmail.com>
2019-06-18 21:52:16 +0200

    refactor: move constants to their own file

---
+++ src/constants.py
@@ -0,0 +1,4 @@
+NOT_FOUND = '404 Not Found'
+TEXT_PLAIN = [('Content-Type', 'text/plain')]
+KEY_NOT_FOUND = [b"Key not found!"]
+FOUND = '302 Found'
--- src/server.py
+++ src/server.py
@@ -3,6 +3,7 @@ import os
 import time
 import hashlib
 
+from src.constants import *
 from src.file_cache import FileCache
 
 # --- MASTER SERVER ---
@@ -23,16 +24,15 @@ def master(env, start_response):
             # TODO: figure out which Volume server to connect to
             pass
 
-            # key does not exist & no interest in creating it
-        start_response('404 Not Found', [('Content-Type', 'text/html')])
-        return [b"Key not found!"]
+        start_response(NOT_FOUND, TEXT_PLAIN)
+        return KEY_NOT_FOUND
 
     # key is found
     meta = json.loads(metakey)
 
     # redirect for either [GET, DELETE]
     headers = [('location', 'https://%s%s' % (meta['volume'], key))]
-    start_response('302 Found', headers)
+    start_response(FOUND, headers)
     db.put(b'key-%d' % time.time(), b'bob')
 
 
@@ -54,5 +54,8 @@ def volume(env, start_response):
     key = env['REQUEST_URI'].encode('utf-8')
     hashkey = hashlib.md5(key).hexdigest()
 
-    if not fc.exists(hashkey):
-        pass
+    if env['REQUEST_METHOD'] == 'GET':
+        if not fc.exists(key):
+            # key not in cache
+            start_response(NOT_FOUND, TEXT_PLAIN)
+            return KEY_NOT_FOUND
