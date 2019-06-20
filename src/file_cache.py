65d86127c6485fa8996204d2e8844577238c30c2
Dragos Strugar <itsdragos@gmail.com>
2019-06-20 15:40:43 +0200

    refactor: add host port and streams for db

--- src/file_cache.py
+++ src/file_cache.py
@@ -1,4 +1,5 @@
 import os
+import hashlib
 import tempfile
 
 
@@ -8,13 +9,16 @@ class FileCache(object):
         self.basedir = os.path.realpath(basedir)
         self.tmpdir = os.path.join(self.basedir, 'tmp')
         os.makedirs(self.tmpdir, exist_ok=True)
+        print("FileCache in %s" % basedir)
 
     def key_to_path(self, key, mkdir_is_ok=False):
         """ given a key returns a path that's 3 levels deep  """
-        path = self.basedir + '/' + key[0:2] + '/' + key[0:4]
+        hashkey = hashlib.md5(key).hexdigest()
+
+        path = self.basedir + '/' + hashkey[0:2] + '/' + hashkey[0:4]
         if not os.path.isdir(path) and mkdir_is_ok:
             os.makedirs(path, exist_ok=True)
-        return os.path.join(path, key)
+        return os.path.join(path, hashkey)
 
     def exists(self, key):
         """ given a key returns if that key is already in cache  """
@@ -23,7 +27,10 @@ class FileCache(object):
     def post(self, key, stream):
         """ given a key and a value insert them into the cache  """
         with tempfile.NamedTemporaryFile(dir=self.tmpdir, delete=False) as f:
+            # TODO: make writing to a file more efficient (do not waste RAM)
             f.write(stream.read())
+            import xattr
+            xattr.setxattr(f.name, 'user.key', key)
             os.rename(f.name, self.key_to_path(key, True))
 
     def delete(self, key):
@@ -32,4 +39,4 @@ class FileCache(object):
 
     def get(self, key):
         """ given a key returns its corresponding value from the cache """
-        return open(self.key_to_path(key), 'rb').read()
+        return open(self.key_to_path(key), 'rb')
--- src/server.py
+++ src/server.py
@@ -22,7 +22,6 @@ if os.environ['TYPE'] == 'master':
         print(v)
 
     import plyvel
-
     db = plyvel.DB(os.environ['DB'], create_if_missing=True)
 
 
@@ -30,18 +29,27 @@ def master(env, start_response):
     key = env['REQUEST_URI']
     metakey = db.get(key.encode('utf-8'))
 
+    if env['REQUEST_METHOD'] == 'POST':
+        file_len = int(env.get('CONTENT_LENGTH', '0'))
+        if file_len > 0:
+            db.put(key.encode('utf-8'), env['wsgi.input'].read(), sync=True)
+        else:
+            db.delete(key.encode('utf-8'))
+        return resp(start_response, OK)
+
     if metakey is None:
         if env['REQUEST_METHOD'] == 'POST':
             # handler for data insertion
             # TODO: volume selection should not be random
             volume = random.choice(volumes)
-
-            # store metakey in db
-            metakey = json.dumps({"volume": volume})
-            db.put(key.encode('utf-8'), metakey.encode('utf-8'))
         else:
             return resp(start_response, NOT_FOUND)
     else:
+        # key is found
+        if env['REQUEST_METHOD'] == 'POST':
+            return resp(start_response, CONFLICT)
+
+        # store metakey in db
         meta = json.loads(metakey.decode('utf-8'))
         volume = meta['volume']
 
@@ -53,9 +61,7 @@ def master(env, start_response):
 # --- VOLUME SERVER ---
 
 if os.environ['TYPE'] == 'volume':
-    import socket
-
-    host = socket.gethostname()
+    host = os.environ['HOST'] + ":" + os.environ['PORT']
 
     # create file cache
     fc = FileCache(os.environ['VOLUME'])
@@ -63,23 +69,25 @@ if os.environ['TYPE'] == 'volume':
 
 def volume(env, start_response):
     key = env['REQUEST_URI'].encode('utf-8')
-    hashkey = hashlib.md5(key).hexdigest()
 
     if env['REQUEST_METHOD'] == 'POST':
+        if fc.exists(key):
+            return resp(start_response, CONFLICT)
+
         file_len = int(env.get('CONTENT_LENGTH', '0'))
         if file_len > 0:
-            fc.post(hashkey, env['wsgi.input'])
+            fc.post(key, env['wsgi.input'])
             return resp(start_response, CREATED)
         else:
             return resp(start_response, LENGTH_REQUIRED)
 
     # key is not in cache, should return not found
-    if not fc.exists(hashkey):
+    if not fc.exists(key):
         return resp(start_response, NOT_FOUND, body=KEY_NOT_FOUND)
 
     if env['REQUEST_METHOD'] == 'GET':
-        return resp(start_response, OK, body=fc.get(hashkey))
+        return resp(start_response, OK, body=fc.get(key).read())
 
     if env['REQUEST_METHOD'] == 'DELETE':
-        fc.delete(hashkey)
+        fc.delete(key)
         return resp(start_response, OK)
