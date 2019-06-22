78d28189b46625473f28f7dda5a91a35703fe900
Dragos Strugar <itsdragos@gmail.com>
2019-06-22 17:02:07 +0200

    chore: modify docker scripts

--- runtest.sh
+++ runtest.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker build -t kvdb -f Dockerfile .
-docker run --hostname localhost -p 3000:3000 -p 3001:3001 -p 3002:3002 --name kvdb --rm kvdb bash -c "cd /tmp && test/bringup.sh"
+docker run --hostname localhost -p 3000:3000 -p 3001:3001 -p 3002:3002 --name kvdb --rm kvdb bash -c "cd /tmp && ./test/bringup.sh"
