2709d787f26f952ab4798b5c43eced18a3644471
Dragos Strugar <itsdragos@gmail.com>
2019-06-24 14:14:57 +0200

    fix: travis-ci error with execution

--- test/bringup.sh
+++ test/bringup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
+chmod +x /tmp/bringup.sh &
+chmod +x /tmp/test.py &
+
 PORT=3001 ./volume /tmp/volume1/ &
 PORT=3002 ./volume /tmp/volume2/ &
 
